e5a0167910786697af1f4ce06cfaafff254165f9
Hossam Gouda <hos.gouda90@gmail.com>
2023-06-24 22:48:16 +0300

    update JS CSS HTML

--- script.js
+++ script.js
@@ -13,18 +13,20 @@ image.onload = function () {
 function drawImage() {
   // ctx.clearRect(0, 0, canvas.width, canvas.height)
   ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-  ctx.font = "70px Cairo";
+  ctx.font = "60px Cairo";
   ctx.fillStyle = "#ffffff";
   ctx.textAlign = "center";
 
-  ctx.fillText(nameInput.value, 1000, 1300);
+  ctx.fillText(nameInput.value, 750, 975);
 }
 
 nameInput.addEventListener("input", function () {
   drawImage();
 });
 
-downloadBtn.addEventListener("click", function () {
-  downloadBtn.href = canvas.toDataURL("image/jpg");
-  downloadBtn.download = "Congrates - " + nameInput.value;
+downloadBtn.addEventListener("click", function downladDrw() {
+  const link = document.createElement("a");
+  link.href = canvas.toDataURL("image/png"); // Set the image data as the link URL
+  link.download = "Congrates - " + nameInput.value; // Set the desired file name
+  link.click();
 });
